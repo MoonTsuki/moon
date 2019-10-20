75c119599a8d61b2e39cc97f3d66c6ff28904537
aloysiuslim <aisp.aloysius@gmail.com>
2019-10-20 21:00:31 +0800

    Updates

---
+++ precache-manifest.fc35215ff6d9bc0d9257bde72ff7a995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c8bf0ca03be3da284703fa7720c5ca",
+    "url": "/MoonTsuki/moon/index.html"
+  },
+  {
+    "revision": "9d85b1fa80c041604bbd",
+    "url": "/MoonTsuki/moon/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "16ff7a16eadd50496bc5",
+    "url": "/MoonTsuki/moon/static/css/main.b4110c3f.chunk.css"
+  },
+  {
+    "revision": "9d85b1fa80c041604bbd",
+    "url": "/MoonTsuki/moon/static/js/2.25b0e5a1.chunk.js"
+  },
+  {
+    "revision": "16ff7a16eadd50496bc5",
+    "url": "/MoonTsuki/moon/static/js/main.8dbf086e.chunk.js"
+  },
+  {
+    "revision": "140e6f0280c2f03a3494",
+    "url": "/MoonTsuki/moon/static/js/runtime-main.bf668924.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MoonTsuki/moon/static/media/logo.25bf045c.svg"
+  }
+]);
