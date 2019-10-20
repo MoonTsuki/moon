7f33104d107b59fbc1012af8a3718a1a03538408
aloysiuslim <aisp.aloysius@gmail.com>
2019-10-20 20:20:41 +0800

    feat: add navbar and logo

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to the Moon!</h1>
         <Button variant='light'>My First Button</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
