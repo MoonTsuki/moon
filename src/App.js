990d1ac19ae34b5c367fb7a3766104c46b08d2f4
aloysiuslim <aisp.aloysius@gmail.com>
2019-11-16 23:13:18 +0800

    feat: added a flipping card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
 import Navbar from './components/navbar/navbar';
 import React from 'react';
 import moonImage from './resources/moon.png'
@@ -14,6 +15,31 @@ function App() {
         <Button variant='light'>My First Button</Button>
         <img src={moonImage} className='animated'></img>
       </header>
+      <body>
+        <div className='flip-card'>
+          <div className='flip-card-inner'>
+            <div className='flip-card-front'>
+            <Card>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text> Testing a long paragraph of nothingness and useless content </Card.Text>
+                <Button variant="danger">Oh no.</Button>
+              </Card.Body>
+            </Card>
+            </div>
+            <div className='flip-card-back'> 
+            <Card>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text> Testing a long paragraph of nothingness and useless content </Card.Text>
+                <Button variant="danger">Oh no.</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          </div>
+        </div>
+
+      </body>
     </div>
   );
 }
